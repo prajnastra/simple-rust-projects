09faf97a28e7b52d77f7aa75acdd7cecd32071c2
prajnastra <coder.abhijit90@gmail.com>
2023-02-02 20:07:37 +0530

    update: caluculator

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -3,10 +3,16 @@ use std::io;
 fn main() {
     let mut num1 = String::new();
     let mut num2 = String::new();
+    let mut sign = String::new();
 
     println!("Enter number: ");
     io::stdin()
         .read_line(&mut num1)
+        .expect("Not able to read input");
+
+    println!("Enter sign: ");
+    io::stdin()
+        .read_line(&mut sign)
         .expect("Not able to read input");
 
     println!("Enter another number: ");
@@ -14,18 +20,23 @@ fn main() {
         .read_line(&mut num2)
         .expect("Not able to read input");
 
-    let num1: i64 = match num1.trim().parse() {
+    let num1: f64 = match num1.trim().parse() {
         Ok(num) => num,
-        Err(_) => 0,
+        Err(_) => 0.0,
     };
-    let num2: i64 = match num2.trim().parse() {
+    let num2: f64 = match num2.trim().parse() {
         Ok(num) => num,
-        Err(_) => 0,
+        Err(_) => 0.0,
+    };
+    let sign = sign.trim();
+
+    let result = match sign {
+        "+" => num1 + num2,
+        "-" => num1 - num2,
+        "*" => num1 * num2,
+        "/" => num1 / num2,
+        _ => 0.0,
     };
 
-
-    println!("{num1} + {num2} = {}", num1 + num2);
-    println!("{num1} - {num2} = {}", num1 - num2);
-    println!("{num1} * {num2} = {}", num1 * num2);
-    println!("{num1} / {num2} = {}", num1 / num2);
+    println!("Result is {result}");
 }
