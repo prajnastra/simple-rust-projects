5a737a1a0f2ef98a086fcc9be8c0e5a0a4fb6fd5
prajnastra <coder.abhijit90@gmail.com>
2023-02-02 14:35:56 +0530

    finish calculator

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut num1 = String::new();
+    let mut num2 = String::new();
+
+    println!("Enter number: ");
+    io::stdin()
+        .read_line(&mut num1)
+        .expect("Not able to read input");
+
+    println!("Enter another number: ");
+    io::stdin()
+        .read_line(&mut num2)
+        .expect("Not able to read input");
+
+    let num1: i64 = match num1.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    };
+    let num2: i64 = match num2.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    };
+
+
+    println!("{num1} + {num2} = {}", num1 + num2);
+    println!("{num1} - {num2} = {}", num1 - num2);
+    println!("{num1} * {num2} = {}", num1 * num2);
+    println!("{num1} / {num2} = {}", num1 / num2);
 }
